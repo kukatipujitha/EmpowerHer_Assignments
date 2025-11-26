e31c02f320da63eaa8a08a7ef52432ddde2594b6
kukatipujitha <pujithakukati11@gmail.com>
2025-11-26 22:06:18 +0530

    ages

---
+++ closures/q19/ages.js
@@ -0,0 +1,18 @@
+
+let age = 20;
+
+
+function displayAge() {
+    console.log("Age is:", age);
+}
+
+
+function changeAge() {
+    age = 25; 
+    console.log("Age after updating:", age);
+}
+
+
+displayAge();   
+changeAge();   
+displayAge();   
